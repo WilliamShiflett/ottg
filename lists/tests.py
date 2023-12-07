53e587f6efe8fcefefc2db5f5dd363b1f0d87900
Will Shiflett <3066319+WilliamShiflett@users.noreply.github.com>
2023-12-07 10:37:22 -0500

    back to original state

--- lists/tests.py
+++ lists/tests.py
@@ -2,9 +2,24 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
+# # write a test you know will fail to ensure test runner is working
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1+1, 3)
+
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
+
+
+# class HomePageTest(TestCase):
+#     def test_home_page_returns_correct_html(self):
+#         response = self.client.get("/")
+#         self.assertContains(response, "<title>To-Do lists</title>")
+#         self.assertContains(response, "<html>")
+#         self.assertContains(response, "</html>")
